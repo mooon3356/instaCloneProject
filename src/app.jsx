1e8f8e2c45b1af9a559160082d6deea3744d7c65
mooon3356 <mooon3356@gmail.com>
2021-04-01 15:08:34 +0900

    edit Readme

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,19 @@ import SignUp from './components/SignUp';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignIn from './components/SignIn';
 
+//react-router-dom에 관해서
 // 1. route의 기능
 // 2. browserRouter, hashRouter의 기능
 // 3. Switch의 기능
 // 4. useHistory,useParams 기능
 
+
+// 남은 것들
+// 1. 유효성 검사
+// 2. facebook이나 naver open api 넣기.
+//
+
+// Advanced. 유저가 로그인 창에 정보를 입력했을 시, 서버의 데이터들과 비교하여 로그인 성공 창을 보여준다.
 
 function App() {
   return (
